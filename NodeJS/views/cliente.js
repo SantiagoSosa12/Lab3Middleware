3e5870c0b3596bd9bcf5084f0632c24ddd7e754b
SantiagoSosaReyes <santiago.sosa@uptc.edu.co>
2021-03-07 22:58:01 -0500

    demo lista pokemons

--- NodeJS/views/cliente.js
+++ NodeJS/views/cliente.js
@@ -2,9 +2,19 @@ function myFunction() {
     fetch('http://192.168.0.16:3000/pokemons')
     .then(response => response.text())
     .then((response) => {
-        console.log(response)
-        document.getElementById("demo").innerHTML = response;
+        console.log(response);
+        showTable(response);
     })
     .catch(err => console.log(err));
   
+}
+
+function showTable(data){
+    let generalList = data.split(";");
+    generalList.forEach(function(elemento) {
+        let particularList = elemento.split(",");
+        particularList.forEach(function(elemento1) {
+            console.log(elemento1);
+        })
+    })
 }
