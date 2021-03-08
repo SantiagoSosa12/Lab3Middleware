2d097edafaac6f3e7f719e8958e4b9f25e876363
SantiagoSosaReyes <santiago.sosa@uptc.edu.co>
2021-03-07 19:39:39 -0500

    Peticion post

--- NodeJS/index.js
+++ NodeJS/index.js
@@ -1,26 +1,25 @@
-
 const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.urlencoded({ extended: true}));
+
+app.use(body_parser.json());
 app.use(express.static(__dirname + "/views"));
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.get('/registrarPokemon', (req, res) => {
-  if (!req.body)
-        return res.sendStatus(400)
-
-    var user = req.body.user;
-    var pass = req.body.pass;
+app.post('/registrarPokemon', (req, res) => {
+  var user = req.body.user;
+  var pass = req.body.pass;
   console.log(request.body.nombrepokemon);
   console.log(request.body.peso);
   console.log(request.body.altura);
   console.log(request.body.tipo);
   res.send('Registrado!!')
-})
+});
 
 
 app.listen(port, () => {
